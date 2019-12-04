bfa2e287518951f8589b52b23aba6bba53fd3324
nomaanulhasan <nomaanulhasan@gmail.com>
2019-12-04 18:25:43 +0530

    App base
    Uploaded base app.

---
+++ demo_uppercase.js
@@ -0,0 +1,8 @@
+﻿var http = require('http');
+var uc = require('upper-case');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  /*Use our upper-case module to upper case a string:*/
+  res.write(uc("Hello World!"));
+  res.end();
+}).listen(8080);
